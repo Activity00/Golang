8bce22f1031eaf28a1be8dfb08b9751aa619bf49
wuminghui <1032662429@qq.com>
2019-04-25 11:18:16 +0800

    WIP: first commit

---
+++ basic/array.go
@@ -0,0 +1,51 @@
+package main
+
+func printArray(array []int) {
+	for i, x := range array {
+		println(i, x)
+	}
+}
+
+func main() {
+	// 申明与初始化
+	var array0 [10]int
+	for i, x := range array0 {
+		println(i, x)
+	}
+
+	// 初始化
+	array1 := [10]int{1, 2, 3, 4, 5}
+	for i, x := range array1 {
+		println(i, x)
+	}
+
+	// 默认长度
+	array2 := []int{1, 2, 4}
+	for i, x := range array2 {
+		println(i, x)
+	}
+
+	array3 := [...]int{1, 2, 4}
+	for i, x := range array3 {
+		println(i, x)
+	}
+
+	//特定赋值
+	array4 := []int{1: 100, 5: 500}
+	for i, x := range array4 {
+		println(i, x)
+	}
+
+	println(array4[1])
+	array4[1] = 1000
+	println(array4[1])
+	println(array4)
+
+	//指针数组
+	array5 := [5]*int{1: new(int), 4: new(int)}
+	*array5[1] = 10
+	*array5[4] = 40
+	for i, x := range array5 {
+		println(i, x)
+	}
+}
