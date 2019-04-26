67363158f1eaea8c2136ad4e1cebe9ff3ea503c3
wuminghui <1032662429@qq.com>
2019-04-26 15:03:07 +0800

    WIP: interface

--- basic/type/structtype.go
+++ basic/type/structtype.go
@@ -1,5 +1,37 @@
 package main
 
-func main() {
+import "fmt"
 
+type notifier interface {
+	notify()
+}
+
+type user1 struct {
+	name  string
+	email string
+}
+
+func (u *user1) notify() {
+	fmt.Println("notify", u.name, u.email)
+}
+
+func sendNotification(n notifier) {
+	n.notify()
+}
+
+type admin struct {
+	name  string
+	email string
+}
+
+func (a *admin) notify() {
+	fmt.Println("admin", a.name, a.email)
+}
+
+func main() {
+	u := user1{name: "wmh", email: "1032662429"}
+	sendNotification(&u)
+
+	a := admin{name: "admin", email: "admin@qq.com"}
+	sendNotification(&a)
 }
